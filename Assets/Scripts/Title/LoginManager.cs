e2951b57bd20141610f92561f4a0d6b8a97cbc15
frod2793 <tmdwhd0344@naver.com>
2025-11-20 00:02:01 +0900

    로그인 세팅

--- Assets/Scripts/Title/LoginManager.cs
+++ Assets/Scripts/Title/LoginManager.cs
@@ -113,7 +113,6 @@ namespace DogGuns_Games
         /// </summary>
         private async UniTaskVoid OnStartButtonPressed()
         {
-            Debug.Log("clike");
             SetInteractable(false); // 중복 클릭 방지
 
             // ServerManager 반환 타입 (bool success, string error) 대응
@@ -137,6 +136,7 @@ namespace DogGuns_Games
         /// </summary>
         private async UniTaskVoid OnGuestLoginButtonPressed()
         {
+            Debug.Log("guest");
             SetInteractable(false);
 
             var (success, error) = await m_serverManager.GuestLoginAsync();
@@ -343,7 +343,6 @@ namespace DogGuns_Games
         private void SetInteractable(bool interactable)
         {
             m_startBtn.interactable = interactable;
-            m_guestLoginBtn.interactable = interactable;
             m_loginBtn.interactable = interactable;
             m_signUpBtn.interactable = interactable;
         }
