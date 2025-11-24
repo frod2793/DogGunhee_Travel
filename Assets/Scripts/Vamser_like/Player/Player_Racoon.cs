9435e0c920d7649777d7431aefa3d77d37cd4e1e
frod2793 <tmdwhd0344@naver.com>
2025-11-24 15:26:52 +0900

    라쿤 , 조개 , 문어등 플레이어블 캐릭터 적용

---
+++ Assets/Scripts/Vamser_like/Player/Player_Racoon.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace DogGuns_Games.vamsir
+{
+    public class Player_Racoon : PlayerBase
+    {
+        private SpriteRenderer m_playerSpriteRenderer;
+
+        private void Awake()
+        {
+            m_playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        // 부모 클래스의 OnCollisionEnter2D를 오버라이드합니다.
+        public override void OnCollisionEnter2D(Collision2D other)
+        {
+            base.OnCollisionEnter2D(other);
+        }
+
+        public override void Player_attack(Vector3 attackAngle)
+        {
+            base.Player_attack(attackAngle);
+            if (WeaphonBase != null)
+            {
+                WeaphonBase.Weaphon_Attack(attackAngle);
+            }
+        }
+
+        public override void Player_Die()
+        {
+            base.Player_Die();
+            // TODO: 너구리 캐릭터 전용 사망 사운드로 교체 필요
+            SoundManager.PlaySound(Sound.SFX, SoundKeys.PlayerDeth, false);
+        }
+        
+        protected override void PlayHitEffect()
+        {
+            // TODO: 너구리 캐릭터 전용 피격 효과 및 사운드로 교체 필요
+            // EffectManager.Instance.PlayImmediateFlashEffect(m_playerSpriteRenderer);
+            SoundManager.PlaySound(Sound.SFX, SoundKeys.playerHit, false);
+        }
+    }
+}
